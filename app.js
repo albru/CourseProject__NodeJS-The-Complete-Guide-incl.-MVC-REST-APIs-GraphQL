9a244231bbf4ec78fcf56ad202e17dc77d2b0f45
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2019-11-12 21:13:36 +0300

    msg

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const errorController = require("./controllers/error");
+
 app.set("view engine", "pug");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const users = require("./routes/users");
 const usersData = require("./routes/add-user");
@@ -17,12 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(users);
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(usersData.routes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const usersData = require('./add-user');
-console.log(usersData.users)
+const usersData = require("./add-user");
 
 const router = express.Router();
 
-router.get('/users', (req,res,next) => {
-    const userName = usersData.users;
-    console.log(userName.length)
-    res.render('users', {user: userName, pageTitle: 'Users', path: '/users'})
-})
+router.get("/users", (req, res, next) => {
+  const userName = usersData.users;
+  res.render("users", { user: userName, pageTitle: "Users", path: "/users" });
+});
 
 module.exports = router;
