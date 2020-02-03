a2c9da3c629dca13f7b730b34050e37134a2fbdf
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2020-02-03 20:53:56 +0300

    work with MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_complete', 'root', 'pw', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://albru:0jFFzg6llT6S842b@cluster0-nnn5l.mongodb.net/test?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
